682dd4fb455b38cc4b4e32ea0398ea08d303bba8
José Luiz <luizsobrinho3000@hotmail.com>
2018-12-27 16:31:20 -0300

    First Version

---
+++ src/model/entites/Invoice.java
@@ -0,0 +1,37 @@
+package model.entites;
+
+public class Invoice {
+
+	private Double basicpayment;
+	private Double tax;
+
+	public Invoice() {
+
+	}
+
+	public Invoice(Double basicpayment, Double tax) {
+
+		this.basicpayment = basicpayment;
+		this.tax = tax;
+	}
+
+	public Double getBasicpayment() {
+		return basicpayment;
+	}
+
+	public void setBasicpayment(Double basicpayment) {
+		this.basicpayment = basicpayment;
+	}
+
+	public Double getTax() {
+		return tax;
+	}
+
+	public void setTax(Double tax) {
+		this.tax = tax;
+	}
+
+	public Double getTotalpayment() {
+		return this.basicpayment + this.tax;
+	}
+}
---
+++ src/model/services/RentalService.java
@@ -0,0 +1,60 @@
+package model.services;
+
+import model.entites.CarRental;
+import model.entites.Invoice;
+
+public class RentalService {
+
+	private Double pricePerDay;
+	private Double pricePerHour;
+
+	private BrazilTaxService taxserive;
+
+	public RentalService(Double pricePerDay, Double pricePerHour, BrazilTaxService taxserive) {
+
+		this.pricePerDay = pricePerDay;
+		this.pricePerHour = pricePerHour;
+		this.taxserive = taxserive;
+	}
+
+	public void processInvoice(CarRental carRental) {
+		long t1 = carRental.getStart().getTime();
+		long t2 = carRental.getFinish().getTime();
+
+		double hours = (double) (t2 - t1) / 1000 / 60 / 60;
+		double basicpayment;
+		if (hours <= 12) {
+			basicpayment = Math.ceil(hours) * pricePerHour;
+		} else {
+			basicpayment = Math.ceil(hours / 24) * pricePerDay;
+		}
+
+		double tax = taxserive.tax(basicpayment);
+		carRental.setInvoice(new Invoice(basicpayment, tax));
+	}
+
+	public Double getPricePerDay() {
+		return pricePerDay;
+	}
+
+	public void setPricePerDay(Double pricePerDay) {
+		this.pricePerDay = pricePerDay;
+	}
+
+	public Double getPricePerHour() {
+		return pricePerHour;
+	}
+
+	public void setPricePerHour(Double pricePerHour) {
+		this.pricePerHour = pricePerHour;
+	}
+
+	public BrazilTaxService getTaxserive() {
+		return taxserive;
+	}
+
+	public void setTaxserive(BrazilTaxService taxserive) {
+		this.taxserive = taxserive;
+	}
+
+}
